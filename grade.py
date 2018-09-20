99ad24d2e7ebb17312d7f98567f341e429b8c9cc
Abhi-Charan <43023264+Abhi-Charan@users.noreply.github.com>
2018-09-20 09:38:39 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,15 @@
+a=int (input("enter the marks"))
+if(a>=90):
+	print ("grade A+")
+elif(a>=80):
+	print("grade A")
+elif(a>=70):
+	print("grade B+")
+elif(a>=60):
+	print("grade B")
+elif(a>=50):
+	print("grade C")
+elif(a>=40):
+	print("grade D")
+else:
+    print("FAILED")
---
+++ table2.py
@@ -0,0 +1,4 @@
+a=int (input("enter a value for the table to be printed"))
+for i in range (1,11):
+	print(a,'*',i,'=',a*i)
+	
