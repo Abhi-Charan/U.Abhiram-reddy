1adbe113d73a2cd156939d65a83aed51c1272bbf
Abhi-Charan <43023264+Abhi-Charan@users.noreply.github.com>
2018-10-11 09:20:00 +0530

    Add files via upload

---
+++ ownprog.py
@@ -0,0 +1,18 @@
+print("hiii welcome to calculator")
+a=input("enter 1 for adding 2 for subtracting 3 for multiplying and 4 for dividing")
+b=input ("enter a value")
+c=input("enter a value")
+if(a==1):
+	d=b+C
+	print("the value of addition is",d)
+elif(a==2):
+	e=b-c
+	print("the value of subtraction is",e)
+elif(a==3):
+	f=b*c
+	print("the value of multiplying is",f)
+elif(a==4):
+	g=b/c
+	print("the value of dividing is",g)
+else:
+	print("you have entered a wrong input")
